2244236d8fe3509c660269bf5cde35d756663e4c
kingisdave <davidfayreal@gmail.com>
2024-03-24 05:23:45 +0100

    updated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Login() {
   return (
-    <main className="max-h-screen w-full mt-20 p-4 flex flex-col items-center justify-center">
-      <div className="min-w-full xsl:min-w-[60vh] p-6 xsl:px-14 xsl:py-20 bg-blue-700 rounded-2xl">
+    <main className="min-h-screen w-full mt-3 p-4 flex flex-col items-center justify-center">
+      <div className="min-w-full xsl:min-w-[60vh] p-6 xsl:px-14 xsl:py-14 bg-blue-700 rounded-2xl">
           <div className="w-full h-full text-slate-200 flex flex-col gap-y-4">
             <h1 className="text-xl font-bold">Login</h1>
             <div className="flex flex-col gap-y-5 w-full">
@@ -30,8 +30,8 @@ export default function Login() {
             </span>
           </div>
       </div>  
-      <div className="bottom-0 left-0 w-full py-10 text-black px-10 bg-white">
-        <p className="text-xs px-5 mb-3">
+      <div className="fixed bottom-0 left-0 w-full py-10 text-black px-10 bg-white">
+        <p className="text-xs px-5 mb-12">
           By clicking "Create Account" above, you acknowledge that you have read and 
           understood and agree to <span className="font-bold px-1">PatientflowHQ's</span> 
           <Link href={'/'} className="underline px-1">Terms & Conditions</Link> and
